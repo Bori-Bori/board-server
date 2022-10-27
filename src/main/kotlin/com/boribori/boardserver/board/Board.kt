b6895f0a83fcd173429cc875e09117a867ca205d
Cha-Young-Ho <jkijki1249@gmail.com>
2022-10-27 16:13:26 +0900

    Feature : BoardEntity 기본 세팅

--- src/main/kotlin/com/boribori/boardserver/board/Board.kt
+++ src/main/kotlin/com/boribori/boardserver/board/Board.kt
@@ -1,14 +1,23 @@
 package com.boribori.boardserver.board
 
 
+import lombok.Builder
+import lombok.Getter
+import java.util.*
 import javax.persistence.*
 
 
+@Getter
+@Builder
 @Entity
 class Board {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long?= null;
+    private val id: UUID = UUID.randomUUID();
+
+    @Column
+    private var title: String? = null;
+
+
 
 }
