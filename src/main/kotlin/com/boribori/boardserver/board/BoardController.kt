2eb74029f438cdf238fe355703ac933fa37175c5
Cha-Young-Ho <jkijki1249@gmail.com>
2022-11-01 15:35:19 +0900

    Feature : book Entity#2

--- src/main/kotlin/com/boribori/boardserver/board/BoardController.kt
+++ src/main/kotlin/com/boribori/boardserver/board/BoardController.kt
@@ -1,9 +1,11 @@
 package com.boribori.boardserver.board
 
 import com.boribori.boardserver.board.dto.DtoOfGetBoard
+import com.boribori.boardserver.board.dto.request.RequestOfGetBooks
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
@@ -13,8 +15,15 @@ class BoardController(
 ) {
 
     @GetMapping("/api/board/{boardId}")
-    fun getBoard(@PathVariable boardId: UUID): UUID{
+    fun getBoard(@PathVariable boardId: String): String{
 
         return boardId;
+    }
+
+    @GetMapping("/api/boards")
+    fun getBoardList(@RequestParam request: RequestOfGetBooks){
+
+
+
     }
 }
