3b6ada02615a8f114b3d4f089231a518153e90ed
Cha-Young-Ho <jkijki1249@gmail.com>
2022-11-13 19:27:11 +0900

    Refactoring : 기타 리팩토링

--- src/main/kotlin/com/boribori/boardserver/board/Board.kt
+++ src/main/kotlin/com/boribori/boardserver/board/Board.kt
@@ -1,45 +1,63 @@
 package com.boribori.boardserver.board
 
 
+import com.boribori.boardserver.board.dto.response.ResponseOfGetBoard
 import com.boribori.boardserver.comment.Comment
+import com.boribori.boardserver.common.ResponseOfGetBookContent
 import java.time.LocalDate
 import javax.persistence.*
 
 
 @Entity
-class Board ( @Id val isbn: String,
+class Board {
+    @Id
+    lateinit var isbn: String
 
-              @Column
-              val title: String? = null,
+    @Column
+    var title: String? = null
 
-              @Column
-              val author: String? = null,
+    @Column
+    var author: String? = null
 
-              @Column
-              var pubDate: LocalDate? = null,
+    @Column
+    var pubDate: LocalDate? = null
 
-              @Column
-              var category1: String? = null,
+    @Column
+    var category1: String? = null
 
-              @Column
-              var category2: String? = null,
+    @Column
+    var category2: String? = null
 
-              @Column
-              var category3: String? = null,
+    @Column
+    var category3: String? = null
 
-              @Column
-              var publisher: String? = null,
+    @Column
+    var publisher: String? = null
 
-              @Column
-              var viewCount: Long? = 0,
+    @Column
+    var viewCount: Long? = 0
 
-              @Column
-              var imagePath: String? = null,
+    @Column
+    var imagePath: String? = null
 
-              @OneToMany(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY, mappedBy = "board")
-              var commentList: MutableList<Comment> = mutableListOf<Comment>()){
+    @OneToMany(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY, mappedBy = "board")
+    var commentList: MutableList<Comment> = mutableListOf<Comment>()
+    fun of(content : ResponseOfGetBookContent) : Board {
+        this.isbn = content.isbn.toString()
+        this.title = content.title
+        this.imagePath = content.imagePath
+        this.author = content.author
+        this.imagePath = content.imagePath
+        this.category1 = content.category1
+        this.category2 = content.category2
+        this.category3 = content.category3
+        this.publisher = content.publisher
+        this.viewCount = 1
+        this.pubDate = content.pubDate
+        this.commentList = mutableListOf()
 
-
+        return this
+    }
 
 
 }
--- src/main/kotlin/com/boribori/boardserver/board/dto/response/ResponseOfGetBoard.kt
+++ src/main/kotlin/com/boribori/boardserver/board/dto/response/ResponseOfGetBoard.kt
@@ -1,29 +1,49 @@
 package com.boribori.boardserver.board.dto.response
 
+import com.boribori.boardserver.board.Board
+import com.boribori.boardserver.common.ResponseOfGetBookContent
 import java.time.LocalDate
 
-data class ResponseOfGetBoard(
-        val isbn: String,
+class ResponseOfGetBoard{
+    var isbn: String? = null
 
-        var title: String? = null,
-
-
-        var author: String? = null,
+    var title: String? = null
 
 
-        var pubDate: LocalDate? = null,
+    var author: String? = null
 
 
-        var category1: String? = null,
+    var pubDate: LocalDate? = null
 
 
-        var category2: String? = null,
+    var category1: String? = null
 
 
-        var category3: String? = null,
+    var category2: String? = null
 
-        var description: String? = null,
-        var publisher : String? = null,
-        var imagePath : String? = null
-) {
+
+    var category3: String? = null
+
+    var description: String? = null
+    var publisher : String? = null
+    var imagePath : String? = null
+    fun of(content : ResponseOfGetBookContent) : ResponseOfGetBoard{
+        this.isbn = content.isbn.toString()
+        this.title = content.title
+        this.imagePath = content.imagePath
+        this.author = content.author
+
+        return this
+    }
+
+    fun of(board : Board) : ResponseOfGetBoard{
+        this.isbn = board.isbn
+        this.title = board.title
+        this.imagePath = board.imagePath
+        this.author = board.author
+
+        return this
+    }
+
+
 }
